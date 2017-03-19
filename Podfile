76964c5011253322c91363514a92292bc592c9d6
k-moto <k.motoyama@smartdt.jp>
2017-03-19 11:27:01 +0900

    add MaterialKit

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ios-materialkit-demo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'MaterialKit', :git => 'https://github.com/faresbh/MaterialKit', :branch => "swift3"
+
+  # Pods for ios-materialkit-demo
+
+  target 'ios-materialkit-demoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ios-materialkit-demoUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ ios-materialkit-demo/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ios-materialkit-demo
+//
+//  Created by k_motoyama on 2017/03/19.
+//  Copyright © 2017年 k_moto. All rights reserved.
+//
+
+import UIKit
+import MaterialKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let materialView = createTextField()
+        self.view.addSubview(materialView)
+        
+    }
+    
+    // マテリアルデザイン風のボタン
+    func createMKButton() -> UIView {
+        
+        let button = MKButton(frame: CGRect(x: 100, y: 100, width: 100, height: 35))
+        button.maskEnabled = true
+        button.setTitle("→", for: .normal)
+        button.backgroundColor = UIColor.MKColor.LightGreen.A700
+        
+        return button
+    }
+
+    // マテリアルデザイン風のテキストフィールド
+    func createTextField() -> UIView {
+        
+        let textField = MKTextField(frame: CGRect(x: 100, y: 100, width: 200, height: 35))
+        textField.floatingPlaceholderEnabled = false
+        textField.placeholder = "place holder"
+        textField.layer.borderColor = UIColor.MKColor.Green.A700.cgColor
+        textField.rippleLayerColor = UIColor.MKColor.LightGreen.A200
+
+        return textField
+    }
+    
+}
+
